68be2269a4f99d839bf70f7076b1f9b413e1bac5
Samuel Stenberg <samuel.stenberg@teokem.lu.se>
2022-04-19 17:45:55 +0200

    .

--- src/timer.h
+++ src/timer.h
@@ -6,24 +6,24 @@
 
 namespace Simulator{
 
-struct Timer{
-    private:
-    std::chrono::steady_clock::time_point start;
-    //std::source_location location;
-    std::string location;
+    struct Timer{
+        private:
+        std::chrono::steady_clock::time_point start;
+        //std::source_location location;
+        std::string location;
 
-    public:
-    Timer(std::string location){
-        start = std::chrono::high_resolution_clock::now();
-        this->location = location;
-    }
+        public:
+        Timer(std::string location){
+            start = std::chrono::high_resolution_clock::now();
+            this->location = location;
+        }
 
-    ~Timer(){
-        std::chrono::duration<float> duration = std::chrono::high_resolution_clock::now() - start;
+        ~Timer(){
+            std::chrono::duration<float> duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
 
-        //std::cout << location.function_name() << " took" << duration.count() << "s" << std::endl;
-        Logger::Log(location, " took ", duration.count(), "s");
-    }
-};
+            //std::cout << location.function_name() << " took" << duration.count() << "s" << std::endl;
+            Logger::Log(location, " took ", duration.count(), "s");
+        }
+    };
 
 }
